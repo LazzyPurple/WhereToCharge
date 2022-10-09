a05a2adf75ef5b08eba23f7213a358763a23e6a2
LazzyPurple <nathan.toto@epitech.digital>
2022-10-09 12:43:34 +0200

    Modified backend

--- BackEnd/server.py
+++ BackEnd/server.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import *
+from pymongo import *
 
 app = Flask(__name__)
 
+try:
+    client = MongoClient(host="localhost", port=27017, serverSelectionTimeoutMS=1000)
+    # client = MongoClient(
+    #     "mongodb+srv://Nathan:IamNathan@wheretocharge.6gnsnno.mongodb.net/"
+    # )
+    # client.server_info()
+    # db = client["WhereToCharge"]
+    # # collections = db["WhereToCharge"]
+except:
+    print("ERROR - Cannot connect to DB")
+
 # Marker API Route
-@app.route("/markers")
-def markers():
-    return {"markers": ["marker 1", "marker 2", "marker3"]}
+@app.route("/markers", methods=["GET"])
+def get_markers():
+    try:
+        data = db.user.find()
+        return data
+    except Exception as ex:
+        print(ex)
+        return Response(
+            response=json.dumps({"message": "cannot read markers from db"}),
+            status=500,
+            mimetype="application/json",
+        )
 
 
 if __name__ == "__main__":
